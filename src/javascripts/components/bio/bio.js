7c41a7ab044dc074f5c5fbbf1a37225d9bdae5ff
Mark Young <markyaaronyoung@gmail.com>
2020-08-29 13:56:12 -0500

    added styling to my landing page

---
+++ src/javascripts/components/bio/bio.js
@@ -0,0 +1,15 @@
+import utils from '../../helpers/utils';
+import 'bootstrap'; 
+
+const buildBio = () => {
+  const domString = `
+    <div class="home">
+      <a id="go-home"><i class="fas fa-arrow-left"></i></a>
+    </div>
+  
+  `;
+  utils.printToDom('#website', domString);
+  $('body').on('click', '#go-home', utils.goHome);
+};
+
+export default { buildBio };
